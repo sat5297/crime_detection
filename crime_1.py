6d9f704c668f9d025e5473d695e5dc901e12307f
Shakti Ratan <38126653+sat5297@users.noreply.github.com>
2020-03-30 20:35:21 +0530

    Update crime_1.py
    The model works as described. The model gets the data as the geo location and the date and time and then using this details the model makes the prediction. This data uses the details and then it works well by taking these details and then makes the prediction as to what are the chances of crime in the region.

--- crime_1.py
+++ crime_1.py
@@ -59,3 +59,50 @@ def generalizeData():
 
 generalizeData()
 
+
+pd.set_option('display.max_columns',None)
+
+X_df = pd.read_csv('./finaalllly.csv')
+
+X_df1 = X_df.iloc[:,1:]
+
+X_df2 = X_df.iloc[:,1:]
+
+y_df = pd.read_excel('./CRIME_CODE.xlsx')
+y_df_cpy = y_df
+x_df_cpy = X_df1
+
+y_df['ID'].value_counts(normalize=True)
+
+X_df2['Location'].dtype
+
+obj_cols = [c for c in X_df2.columns if X_df2[c].dtype == 'O']
+
+for c in obj_cols:
+    X_df2[c] = pd.factorize(X_df2[c])[0]
+    
+feats = [c for c in X_df2.columns if c != 'OFFENSE_CODE']
+
+X = X_df2[feats]
+y = X_df2['OFFENSE_CODE']
+
+from sklearn.preprocessing import scale
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.75, test_size=0.25, random_state=42, shuffle=True)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import normalize
+
+logreg = LogisticRegression()
+logreg.fit(X_train,y_train)
+
+preds = logreg.predict(X_test)
+
+accuracy_score(y_test, preds)
+##We got the accuracy of 78% .
+
+
+
